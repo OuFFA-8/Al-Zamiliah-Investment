48e77e82db773046adb9b4960254df047b0cc224
mustafa ramadan <mustafa.ramadan165@gmail.com>
2026-06-11 22:52:12 +0300

    fix: update body size limit for server actions in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      bodySizeLimit: '50mb',
+      bodySizeLimit: '10mb',
     },
   },
   images: {
